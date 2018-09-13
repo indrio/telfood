bdcbf876997fd64cacaf0a26b2ea8a8a03f88e13
Indrio <indrio@gmail.com>
2018-09-14 03:15:09 +0700

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
 @NgModule({
   declarations: [
@@ -28,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+
+/**
+ * Generated class for the SignupPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+    createSuccess = false;
+    registerCredentials = { email: '', password: '' };
+    
+    constructor(private nav: NavController, private auth: AuthServiceProvider, private alertCtrl: AlertController) { }
+    
+    public login() {
+        this.nav.pop();
+    }
+    
+    public register() {
+        this.auth.register(this.registerCredentials).subscribe(success => {
+            if (success) {
+                this.createSuccess = true;
+                this.showPopup("Success", "Account created.");
+            } else {
+                this.showPopup("Error", "Problem creating account.");
+            }
+        },
+        error => {
+            this.showPopup("Error", error);
+        });
+    }
+
+    showPopup(title, text) {
+        let alert = this.alertCtrl.create({
+            title: title,
+            subTitle: text,
+            buttons: [
+                {
+                    text: 'OK',
+                    handler: data => {
+                        if (this.createSuccess) {
+                            this.nav.popToRoot();
+                        }
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
+}
